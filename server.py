80bd9adcd49a1c6249d76b8bca91bac7b76b659e
jhorelrevilla <jhorel.revilla@ucsp.edu.pe>
2022-10-06 13:16:47 -0500

    cambio de rutas y css

--- server.py
+++ server.py
@@ -1,31 +1,41 @@
 
 import os
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request,redirect, url_for, jsonify
 from load_pages import *
 
 app = Flask(__name__)
 
 ranks = hashmap_pagerank()
 
+
 @app.route("/")
 def home():	
 	return render_template('index.html')
 
+@app.route("/original")
+def homeOrginal():	
+	return render_template('indexOriginal.html')
+
 @app.route("/search", methods=["POST"])        
 def search():
-#	global inverted_index
 	page_rank = {}
 
-	data = request.json
-	words =  data["words"]
+	words = request.form['busqueda']
+	
+	#print(f"recibido: '{words}'")
 
 	words = words.lower().split()
 
+	#Realizar busqueda
 	response  = search_query(words)
 
+	print(response)
+	print("--------------------------")
+	#lista almacenar resultados
 	pagelist = []
 
 	if not len(response):
+		print("No encontro nada")
 		return jsonify([])
 
 	else:
@@ -45,25 +55,14 @@ def search():
 
 		page_rank = {k: v for k, v in sorted(page_rank.items(), reverse=True, key=lambda item: item[1][0])}		
 
-		if len(page_rank)<=10:
-			for key in page_rank:
-			    temp = [key,page_rank[key]]
-			    pagelist.append(temp)
+		for key in page_rank:
+			temp = [key, page_rank[key]]
+			pagelist.append(temp)
 
-			return jsonify(pagelist)
-				
-		else:				
-			temp_dic = dict(list(page_rank.items())[:10])	
-			for key in temp_dic:
-			    temp = [key,temp_dic[key]]
-			    pagelist.append(temp)	
-
-			return jsonify(pagelist)
-
+		print(pagelist)
+		return jsonify(pagelist)
 	#os.system('bash java.sh '+words)
 	#print (response)
-	
-	
 
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0', port=5000)
+	app.run(debug = True, host='0.0.0.0', port=5000)
